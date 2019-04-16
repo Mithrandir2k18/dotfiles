f9804d204c7c669982d64b4ffb21fc14cd677f18
Mithrandir2k18 <themithrandir09@gmail.com>
2019-04-16 14:01:58 +0200

    fix git links

--- vim-init.sh
+++ vim-init.sh
@@ -5,12 +5,14 @@ echo "Installing vim and used plugins..."
 echo "Assuming dotfiles repo is located in ~/dotfiles"
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-sudo apt update && apt upgrade -y
+sudo apt update 
+sudo apt upgrade -y
 sudo apt install vim cscope -y
 
 echo "Cloning plungins..."
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/vim-scripts/taglist.vim ~/.vim/bundle/
-git clone https://github.com/tpope/vim-surround ~/.vim/bundle/
-git clone https://github.com/tpope/vim-repeat ~/.vim/bundle/
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/vim-scripts/taglist.vim.git ~/.vim/bundle/taglist-vim
+git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround 
+git clone https://github.com/tpope/vim-repeat.git ~/.vim/bundle/vim-repeat 
 
