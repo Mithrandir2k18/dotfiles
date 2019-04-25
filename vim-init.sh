b8e02eca248b05e0e1828e0ce082f131f021d855
Mithrandir2k18 <themithrandir09@gmail.com>
2019-04-25 13:41:59 +0200

    fix typo and add CamelCaseMotion plugin

--- vim-init.sh
+++ vim-init.sh
@@ -9,10 +9,11 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install vim cscope -y
 
-echo "Cloning plungins..."
+echo "Cloning plugins..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/vim-scripts/taglist.vim.git ~/.vim/bundle/taglist-vim
 git clone https://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround 
 git clone https://github.com/tpope/vim-repeat.git ~/.vim/bundle/vim-repeat 
+git clone https://github.com/bkad/CamelCaseMotion.git ~/.vim/bundle/CamelCaseMotion.git
 
