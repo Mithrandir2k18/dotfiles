197913c7c042098514cf208bcc843238127d7472
Mithrandir2k18 <themithrandir09@gmail.com>
2023-11-06 13:38:25 +0100

    Check if I like letters better than symbols

--- neovim/lua/plugins/lsp/lspconfig.lua
+++ neovim/lua/plugins/lsp/lspconfig.lua
@@ -71,7 +71,8 @@ return {
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
 		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
