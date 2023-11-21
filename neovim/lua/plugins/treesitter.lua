42d6605fba194368b31c8d42cfee8ed5beb02524
Mithrandir2k18 <themithrandir09@gmail.com>
2023-11-21 15:10:32 +0100

    fix: temp fix for broken repeat action

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -172,9 +172,10 @@ return {
 		)
 
 		-- make builtin f, F, t, T also repeatable with ; and ,
-		vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-		vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-		vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-		vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+        -- disable until https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/519 is fixed
+		-- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+		-- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+		-- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+		-- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
 	end,
 }
