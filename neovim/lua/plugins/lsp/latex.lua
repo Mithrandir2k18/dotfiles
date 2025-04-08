cbfe0df22ac3b9a6063b491fa816b3d17bd9b945
Mithrandir2k18 <themithrandir09@gmail.com>
2025-04-08 17:24:19 +0200

    chore: update plugins, switch pdfviewer
    Also change where zenmode bindings are set for lazyloading

--- neovim/lua/plugins/lsp/latex.lua
+++ neovim/lua/plugins/lsp/latex.lua
@@ -5,7 +5,7 @@ return {
     -- needs tree-sitter-cli. install with 'cargo install tree-sitter-cli'
 	init = function()
 		-- VimTeX configuration goes here, e.g.
-		vim.g.vimtex_view_method = "sioyek"
+		vim.g.vimtex_view_method = "mupdf"
 		vim.keymap.set({ "n" }, "<leader>ttc", ":call vimtex#compiler#compile()<CR>", { desc = "Toggle Tex Compile" })
 	end,
 }
